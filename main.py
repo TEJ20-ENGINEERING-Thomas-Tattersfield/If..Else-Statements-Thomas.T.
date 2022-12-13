60ff09ec416355b78d5146a58affd00f00737bd1
Thomas Tattersfield <118993239+Thomas-Tatt@users.noreply.github.com>
2022-12-13 15:19:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from random import * 
+
+random_number = randint (1,6) # sets the random number  
+
+input ("Please select a number from 1 to 6: ") 
+
+a = randint(1, 5)
+
+if a < 3:
+
+ print("The number is small: %d" %(a))
+
+if a > 3:
+
+ print("Hi,the number is big: %d" %(a))
+
+if a == 3:
+
+ print("The number is exactly: %d" %(a))
